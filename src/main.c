d85bc12e77dec1a65c24493234ac542d966cc83b
Bouter <davidbos@live.be>
2017-07-03 09:58:00 +0200

    Added feature

--- src/main.c
+++ src/main.c
@@ -11,15 +11,25 @@
 
 #include "stm32f4xx.h"
 			
-
+char test(int getal);
+char feature(int getal);
+char test2(int getal);
 int main(void)
 {
-	int test;
+	//int test;
 	char res;
 	res = test(4);
+	res = res + 'r';
 	for(;;);
 }
-
+char feature(int getal)
+{
+	if(getal == 5)
+	{
+		return 'a';
+	}
+	return 'b';
+}
 char test(int getal)
 {
 	if(getal == 5)
